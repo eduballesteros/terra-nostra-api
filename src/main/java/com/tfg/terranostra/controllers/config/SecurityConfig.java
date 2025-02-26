466f40bf527c078df8249066d0449279f67054a5
ROGSTRIX-DE-EDU\balle <ballesterospereze@gmail.com>
2025-02-26 18:33:58 +0100

    Update SecurityConfig to use AuthenticationManager and modify endpoint path for user registration

--- src/main/java/com/tfg/terranostra/controllers/config/SecurityConfig.java
+++ src/main/java/com/tfg/terranostra/controllers/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.tfg.terranostra.controllers.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     /**
@@ -28,24 +28,13 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())  // Deshabilitar CSRF si es necesario
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/registro").permitAll()
+                        .requestMatchers("/api/usuario/registro").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());  // Nueva forma recomendada
 
         return http.build();
     }
-
-    /**
-     * Método de encriptación de contraseña
-     * @return
-     * @author ebp 30/01/25
-     */
-    @Bean
-    public PasswordEncoder contraseniaEncriptada() {
-        return new BCryptPasswordEncoder();
-    }
-
 
 
 }
